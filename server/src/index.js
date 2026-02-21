76a7d2bb88d56c6581e9fb413093ebbb37b1739b
Tamanna-12367 <tamannaalan123@gmail.com>
2026-02-21 11:01:22 +0530

    Initial commit

--- server/src/index.js
+++ server/src/index.js
@@ -11,6 +11,11 @@ app.get('/owner', (req, res) =>{
 res.json({ owner: "Ramesh Kaka" });
 });
 
+app.get('/customers/:id', (req, res) => {
+const customerId = req.params.id;
+res.send(`Looking for Customer ID: ${customerId}`);
+});
+
 app.listen(5001,()=>{
     console.log("🚀Counter Running at port 5001");
 });
